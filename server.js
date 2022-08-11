9568f01d5fc4db6b1d5606c67f9b951dab702818
igor antonio <igorantonio052000@gmail.com>
2022-08-11 00:04:55 -0300

    teste heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/exercicio-AN-07'));
 
 app.get('/', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/exercicio-AN-07/index.html'));
+res.sendFile(path.join(__dirname+'/dist/exercicio-AN-07/src/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
